b654c3c28409f97a44968f1df6c9322ae3971dd3
Erick Jovan <erickjovan106@gmail.com>
2023-11-17 16:35:10 -0600

    Disable FK of stores table

--- WebApp/Scripts/stores/create_store_tables.sql
+++ WebApp/Scripts/stores/create_store_tables.sql
@@ -8,16 +8,16 @@ CREATE TABLE [stores].[store] (
     ,CONSTRAINT [pk_store] PRIMARY KEY CLUSTERED ( [store_id] ASC )
 );
 
-ALTER TABLE [stores].[store] WITH CHECK ADD CONSTRAINT [fk_store_store_format] FOREIGN KEY([store_format_id])
-REFERENCES [stores].[store_format] ([store_format_id]);
+-- ALTER TABLE [stores].[store] WITH CHECK ADD CONSTRAINT [fk_store_store_format] FOREIGN KEY([store_format_id])
+-- REFERENCES [stores].[store_format] ([store_format_id]);
 
-ALTER TABLE [stores].[store] CHECK CONSTRAINT [fk_store_store_format];
+-- ALTER TABLE [stores].[store] CHECK CONSTRAINT [fk_store_store_format];
 
 
-ALTER TABLE [stores].[store] WITH CHECK ADD CONSTRAINT [fk_store_region] FOREIGN KEY([region_id])
-REFERENCES [regions].[region] ([region_id]);
+-- ALTER TABLE [stores].[store] WITH CHECK ADD CONSTRAINT [fk_store_region] FOREIGN KEY([region_id])
+-- REFERENCES [regions].[region] ([region_id]);
 
-ALTER TABLE [stores].[store] CHECK CONSTRAINT [fk_store_region];
+-- ALTER TABLE [stores].[store] CHECK CONSTRAINT [fk_store_region];
 
 
 ALTER TABLE [stores].[store] WITH CHECK ADD CONSTRAINT [fk_store_state] FOREIGN KEY([state_id])
@@ -35,12 +35,12 @@ CREATE TABLE [stores].[store_media] (
     ,CONSTRAINT [pk_store_media] PRIMARY KEY CLUSTERED ( [store_media_id] ASC )
 );
 
-ALTER TABLE [stores].[store_media] WITH CHECK ADD CONSTRAINT [fk_store_media_media] FOREIGN KEY([media_id])
-REFERENCES [media].[media] ([media_id]);
+-- ALTER TABLE [stores].[store_media] WITH CHECK ADD CONSTRAINT [fk_store_media_media] FOREIGN KEY([media_id])
+-- REFERENCES [media].[media] ([media_id]);
 
-ALTER TABLE [stores].[store_media] CHECK CONSTRAINT [fk_store_media_media];
+-- ALTER TABLE [stores].[store_media] CHECK CONSTRAINT [fk_store_media_media];
 
-ALTER TABLE [stores].[store_media] WITH CHECK ADD CONSTRAINT [fk_store_media_store] FOREIGN KEY([store_id])
-REFERENCES [stores].[store] ([store_id]);
+-- ALTER TABLE [stores].[store_media] WITH CHECK ADD CONSTRAINT [fk_store_media_store] FOREIGN KEY([store_id])
+-- REFERENCES [stores].[store] ([store_id]);
 
-ALTER TABLE [stores].[store_media] CHECK CONSTRAINT [fk_store_media_store];
+-- ALTER TABLE [stores].[store_media] CHECK CONSTRAINT [fk_store_media_store];
