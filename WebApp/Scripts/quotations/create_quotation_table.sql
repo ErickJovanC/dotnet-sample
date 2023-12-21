cb7758d063592be3826a29662296d2dc72bdf82f
Erick Jovan <erickjovan106@gmail.com>
2023-12-20 18:02:14 -0600

    add quotation script

--- WebApp/Scripts/quotations/create_quotation_table.sql
+++ WebApp/Scripts/quotations/create_quotation_table.sql
@@ -1,22 +1,28 @@
+CREATE TABLE [quotations].[quotation_status] (
+    [quotation_status_id]   INT             NOT NULL    identity(1,1)
+    , [title]               NVARCHAR(50)    NOT NULL
+    , [description]         NVARCHAR(256)       NULL
+    , CONSTRAINT [pk_quotation_status] PRIMARY KEY CLUSTERED ( [quotation_status_id] ASC )
+);
+
 CREATE TABLE [quotations].[quotation] (
-    [quotation_id]      INT     NOT NULL    identity(1,1)
-    , [customer_id]     INT     NOT NULL
-    ,CONSTRAINT [pk_quotation] PRIMARY KEY CLUSTERED ( [quotation_id] ASC )
+    [quotation_id]              INT     NOT NULL    identity(1,1)
+    , [tenant_id]               INT     NOT NULL
+    , [quotation_status_id]     INT     NOT NULL
+    , [agency_id]               INT         NULL
+    , [brand_id]                INT         NULL
+    , [discount]                DECIMAL     NULL
+    , [date_start]              DATE        NULL
+    , [date_end]                DATE        NULL
+    , CONSTRAINT [pk_quotation] PRIMARY KEY CLUSTERED ( [quotation_id] ASC )
 );
 
-CREATE TABLE [quotations].[quotation_request] (
-    [quotation_id]     INT     NOT NULL    identity(1,1)
-    , [request_id]     INT     NOT NULL
-    ,CONSTRAINT [pk_quotation_request] PRIMARY KEY CLUSTERED ( [quotation_id] ASC, [request_id] ASC )
-);
 
-ALTER TABLE [quotations].[quotation] WITH CHECK ADD CONSTRAINT [fk_quotation_customer] FOREIGN KEY([customer_id])
-REFERENCES [customers].[customer] ([customer_id]);
-
-ALTER TABLE [quotations].[quotation] CHECK CONSTRAINT [fk_quotation_customer];
+ALTER TABLE [quotations].[quotation] WITH CHECK ADD CONSTRAINT [fk_quotation_tenant] FOREIGN KEY([tenant_id])
+REFERENCES [tenants].[tenant] ([tenant_id]);
+ALTER TABLE [quotations].[quotation] CHECK CONSTRAINT [fk_quotation_tenant];
 
 
-ALTER TABLE [quotations].[quotation_request] WITH CHECK ADD CONSTRAINT [fk_quotation_request_request] FOREIGN KEY([request_id])
-REFERENCES [requests].[request] ([request_id]);
-
-ALTER TABLE [quotations].[quotation_request] CHECK CONSTRAINT [fk_quotation_request_request];
+ALTER TABLE [quotations].[quotation] WITH CHECK ADD CONSTRAINT [fk_quotation_quotation_status] FOREIGN KEY([quotation_status_id])
+REFERENCES [quotations].[quotation_status] ([quotation_status_id]);
+ALTER TABLE [quotations].[quotation] CHECK CONSTRAINT [fk_quotation_quotation_status];
